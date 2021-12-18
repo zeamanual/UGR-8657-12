8e5aa19e09e7a094a7821691189f8c8943373601
zeamanual <zeamanualfeleke@gmail.com>
2021-12-18 23:28:24 +0300

    the layout of the all the pages have completed

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,28 @@
-let navMenu = document.querySelector('.nav-menu');
-let menuButton = document.querySelector('.menu-button');
+let navMenu = document.querySelector('.navigation__menu');
+let menuButton = document.querySelector('.navigation__button');
 menuButton.addEventListener('click',()=>{
     const visibility = navMenu.getAttribute('data-visible')
         navMenu.classList.toggle('visible')
 
+})
+
+let leftSlider = document.querySelector('.card-container__slider--left-side')
+let rightSlider = document.querySelector('.card-container__slider--right-side')
+let cardContainer = document.querySelector('.card-container__inner-container')
+let position = 0;
+leftSlider.addEventListener('click',()=>{
+    if(position==0){
+    }else{
+        position += 30
+        cardContainer.style.transform = `translatex(${position}vw)`
+    }
+})
+rightSlider.addEventListener('click',()=>{
+    if(position < -30){
+
+    }else{
+        position -= 30
+        cardContainer.style.transform = `translatex(${position}vw)`
+    }
+   
 })
